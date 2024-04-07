4a0faa4c78792c5e0d36a530d7b7778057f47797
RudStepan <1gooder2@gmail.com>
2024-04-07 12:58:20 +0700

    Attestation_Java_one

---
+++ untitled/src/main/java/org/example/Programm.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.Controller.PetController;
+import org.example.Model.Pet;
+import org.example.Services.IRepository;
+import org.example.Services.PetRepository;
+import org.example.UserInterface.ConsoleMenu;
+
+public class Programm {
+    public static void main(String[] args) throws Exception {
+
+        IRepository<Pet> myFarm = new PetRepository();
+        PetController controller = new PetController(myFarm);
+        new ConsoleMenu(controller).start();
+    }
+}
